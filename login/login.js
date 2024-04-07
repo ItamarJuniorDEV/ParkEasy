b86e2b886f056501d9212e3ae9c4efdd165b3fa7
Itamar Junior <cdajuniorf@gmail.com>
2024-04-07 18:58:32 -0300

    Foi implementado login no sistema

---
+++ login/login.js
@@ -0,0 +1,15 @@
+const usuario = "admin"
+const password = "admin"
+
+function login(){
+    var login = document.getElementById("usuario").value
+    var senha = document.getElementById("senha").value
+    if(login == usuario && senha == password) {
+        window.location.href="/index.html"
+        window.alert("Login realizado com sucesso")
+
+    }
+    else{
+        window.alert("Usuário ou senha incorreta")
+    }
+}
